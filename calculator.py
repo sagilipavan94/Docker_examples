7048eae09427a571feb735d2d5ec26cc21dae896
sagilipavan94 <150500123+sagilipavan94@users.noreply.github.com>
2023-12-11 10:08:41 +1300

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1=5
+num2=2
+def add():
+    add=num1+num2
+    print(add)
+def sub():
+    sub=num1-num2
+    print(sub)
+def mul():
+    mul=num1*num2
+    print(mul)
+def div():
+    div=num1/num2
+    print(div)
+
+add();
+sub();
+mul();
+div();
